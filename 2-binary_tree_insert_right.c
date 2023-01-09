58c5383e5c4472f9a71ddbf4085b64814bff595d
OmarElati <5796@holbertonstudents.com>
2023-01-09 15:20:51 +0100

    inserts a node as the right-child

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -19,7 +19,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (!new_node)
 		return (NULL);
 
-	if (parent->left != NULL)
+	if (parent->right != NULL)
 	{
 		new_node->right = parent->right;
 		new_node->right->parent = new_node;
