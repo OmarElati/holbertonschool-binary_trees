4092afd9590cd0bb8a16cd0b69be1cb80ded7ade
OmarElati <5796@holbertonstudents.com>
2023-01-10 16:21:29 +0100

    Delete tree

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -10,7 +10,8 @@ void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree)
 	{
-		binary_tree_delete(tree->parent);
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
 		free(tree);
 	}
 }
