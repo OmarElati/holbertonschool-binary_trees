35c2302784eaea3c0a25b0c252f1c0cdf48df85c
OmarElati <5796@holbertonstudents.com>
2023-01-10 19:12:30 +0100

    binary tree using post-order traversal

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -9,9 +9,9 @@
 */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (!tree || !func)
+	if (!tree || !func)
 		return;
 	binary_tree_postorder(tree->left, func);
 	binary_tree_postorder(tree->right, func);
-    func(tree->n);
+	func(tree->n);
 }
