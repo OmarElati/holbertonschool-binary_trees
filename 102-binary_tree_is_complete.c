19d1105d888d7d65b7eea5e8798d868b53f701e1
OmarElati <5796@holbertonstudents.com>
2023-01-13 18:59:36 +0100

    checks if a binary tree is complete

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -8,11 +8,12 @@
 */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-	int left_height = binary_tree_height(tree->left);
-	int right_height = binary_tree_height(tree->right);
+	int left_height, right_height;
 
 	if (!tree)
 		return (0);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
 	if (left_height == right_height)
 		return (binary_tree_is_complete(tree->left) && binary_tree_is_complete(tree->right));
