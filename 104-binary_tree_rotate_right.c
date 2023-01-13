b40235f5c20683e20ecbb784895c36f3ca25e474
OmarElati <5796@holbertonstudents.com>
2023-01-13 22:06:43 +0100

    performs a right-rotation on a binary tree

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -8,22 +8,22 @@
 */
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
-    binary_tree_t *new_root = tree->left;
+	binary_tree_t *new_root = tree->left;
 
-    if (!tree)
-        return (NULL);
+	if (!tree)
+		return (NULL);
 
-    if (!new_root)
-        return (tree);
+	if (!new_root)
+		return (tree);
 
-    tree->left = new_root->right;
+	tree->left = new_root->right;
 
-    if (new_root->right)
-        new_root->right->parent = tree;
+	if (new_root->right)
+		new_root->right->parent = tree;
 
-    new_root->parent = tree->parent;
-    new_root->right = tree;
-    tree->parent = new_root;
+	new_root->parent = tree->parent;
+	new_root->right = tree;
+	tree->parent = new_root;
 
-    return (new_root);
+	return (new_root);
 }
