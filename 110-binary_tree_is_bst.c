88f287a07f75154e262593bdf7cf77715503b03f
OmarElati <5796@holbertonstudents.com>
2023-01-13 22:52:14 +0100

    checks if binary tree is valid Binary Search Tree

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -10,17 +10,17 @@
 */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-    if (!tree)
-        return (0);
-    if (tree->left)
-    {
-        if (tree->n <= tree->left->n || !binary_tree_is_bst(tree->left))
-            return (0);
-    }
-    if (tree->right)
-    {
-        if (tree->n >= tree->right->n || !binary_tree_is_bst(tree->right))
-            return (0);
-    }
-    return (1);
+	if (!tree)
+		return (0);
+	if (tree->left)
+	{
+		if (tree->n <= tree->left->n || !binary_tree_is_bst(tree->left))
+			return (0);
+	}
+	if (tree->right)
+	{
+		if (tree->n >= tree->right->n || !binary_tree_is_bst(tree->right))
+			return (0);
+	}
+	return (1);
 }
