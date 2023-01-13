7db7bf05e052826d32913d924fe38f33a61c1ca3
OmarElati <5796@holbertonstudents.com>
2023-01-13 15:24:46 +0100

    finds the uncle of a node

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -10,9 +10,9 @@
 */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-    if (!node || !node->parent || !node->parent->parent)
-        return (NULL);
-    return (binary_tree_sibling(node->parent));
+	if (!node || !node->parent || !node->parent->parent)
+		return (NULL);
+	return (binary_tree_sibling(node->parent));
 }
 
 /**
