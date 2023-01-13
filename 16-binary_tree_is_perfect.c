63f983e4ce48fef245893af14ff3cbd5dd3a407d
OmarElati <5796@holbertonstudents.com>
2023-01-13 14:50:41 +0100

    checks if a binary tree is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -15,6 +15,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (binary_tree_is_full(tree))
 		if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
 			return (1);
+
 	return (0);
 }
 
