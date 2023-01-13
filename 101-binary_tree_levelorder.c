46b69b3844f193c3f589d50e0e6072e959130cbd
OmarElati <5796@holbertonstudents.com>
2023-01-13 18:19:35 +0100

    binary tree using level-order traversal

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -44,7 +44,7 @@ void print_Level(const binary_tree_t *root, int level, void (*func)(int))
  * binary_tree_height - Function measures the height of a binary tree
  * @tree:  pointer to the root node of the tree to measure the height
  *
- * Return: If tree is NULL, your function must return 0
+ * Return: If tree is NULL, function must return 0
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
