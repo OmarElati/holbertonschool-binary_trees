71ac1361ba3e02492c5fdebabfdba29ee034a898
OmarElati <5796@holbertonstudents.com>
2023-01-13 21:19:26 +0100

    checks if a binary tree is complete

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -8,22 +8,22 @@
 */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-    binary_tree_t *new_root = tree->right;
+	binary_tree_t *new_root = tree->right;
 
-    if (!tree)
-        return (NULL);
+	if (!tree)
+		return (NULL);
 
-    if (!new_root)
-        return (tree);
+	if (!new_root)
+		return (tree);
 
-    tree->right = new_root->left;
+	tree->right = new_root->left;
 
-    if (new_root->left)
-        new_root->left->parent = tree;
+	if (new_root->left)
+		new_root->left->parent = tree;
 
-    new_root->parent = tree->parent;
-    new_root->left = tree;
-    tree->parent = new_root;
+	new_root->parent = tree->parent;
+	new_root->left = tree;
+	tree->parent = new_root;
 
-    return (new_root);
+	return (new_root);
 }
