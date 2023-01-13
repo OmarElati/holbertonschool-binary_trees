563a0e3ab409508f75f264099b12f5288c54514c
OmarElati <5796@holbertonstudents.com>
2023-01-13 13:39:15 +0100

    counts the leaves in a binary tree

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -9,9 +9,9 @@
 */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-    if (!tree)
-        return (0);
-    if (!tree->left && !tree->right)
-        return (1);
-    return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
