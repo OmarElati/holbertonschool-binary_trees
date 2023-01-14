0bd0c13b55d746ae3698da03bf9e04b6c3cdbf8e
OmarElati <5796@holbertonstudents.com>
2023-01-14 13:40:22 +0100

    checks if binary tree valid AVL Tree

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -1,32 +1,50 @@
 #include "binary_trees.h"
 
 /**
- * 
- * 
- * 
- * 
+ * binary_tree_is_avl - Function checks if binary tree valid AVL Tree
+ * @tree: pointer to the root node of the tree to check
+ *
+ * Return: 1 if tree is a valid AVL Tree, and 0 otherwise
+ *         If tree is NULL, return 0
 */
+int binary_tree_is_avl(const binary_tree_t *tree)
+{
+    int left_height, right_height;
 
-int max(int a, int b) {
-    return (a > b) ? a : b;
-}
-
-int height(const binary_tree_t *tree) {
     if (!tree)
-        return 0;
+        return (0);
 
-    return 1 + max(height(tree->left), height(tree->right));
-}
-
-int binary_tree_is_avl(const binary_tree_t *tree) {
-    if (!tree)
-        return 0;
-
-    int left_height = height(tree->left);
-    int right_height = height(tree->right);
+    left_height = height(tree->left);
+    right_height = height(tree->right);
 
     if (abs(left_height - right_height) > 1)
-        return 0;
+        return (0);
 
-    return binary_tree_is_avl(tree->left) && binary_tree_is_avl(tree->right);
+    return (binary_tree_is_avl(tree->left) && binary_tree_is_avl(tree->right));
+}
+
+/**
+ * max - function checks a or b is bigger
+ * @a: input
+ * @b: input
+ *
+ * Return: a OR b
+*/
+int max(int a, int b)
+{
+    return ((a > b) ? a : b);
+}
+
+/**
+ * height - Function calculates the height of the subtree
+ * @tree: pointer to the root node of the tree to check
+ *
+ * Return: 
+*/
+int height(const binary_tree_t *tree)
+{
+    if (!tree)
+        return (0);
+
+    return (1 + max(height(tree->left), height(tree->right)));
 }
